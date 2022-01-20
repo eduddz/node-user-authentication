4f107f613f5d3b66eb4b1265e649931e78a2bb32
eduddz <esteves-dorta@hotmail.com>
2022-01-20 14:19:33 -0300

    Finalizando o projeto

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -20,8 +20,15 @@ usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, n
 
 usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
     const newUser = req.body
-
     res.status(StatusCodes.CREATED).send(newUser)
+})
+
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid
+    const usuario = req.body
+
+    usuario.uuid = uuid
+    res.status(StatusCodes.OK).send({ usuario })
 })
 
 export default usersRoute;
